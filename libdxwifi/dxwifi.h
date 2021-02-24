df54a95b2284fc31e75f695531e78c318fa5daac
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-23 22:41:49 -0800

    Add dxwifi project header and transmitter header
    dxwifi.h defines project wide definitions and constants
    transmitter.h defines the public API for the dxwifi transmitter.

---
+++ libdxwifi/dxwifi.h
@@ -0,0 +1,39 @@
+/**
+ * DxWiFi project includes, definitions, and types
+ * 
+ * https://github.com/oresat/oresat-dxwifi-software
+ * 
+ * GPL-3.0 License
+ */
+
+#ifndef LIBDXWIFI_H
+#define LIBDXWIFI_H
+
+/************************
+ *  Constants
+ ***********************/
+
+// https://www.tcpdump.org/manpages/pcap.3pcap.html
+#define DXWIFI_SNAPLEN_MAX 65535
+
+#define DXWIFI_DFLT_PACKET_BUFFER_TIMEOUT 20
+
+#define DXWIFI_FRAME_CONTROL_DATA_SIZE 256
+#define DXWIFI_FRAME_CONTROL_CHECK_THRESHOLD 0.75
+
+/************************
+ *  Types
+ ***********************/
+
+/**
+ * Control frames are used to signal file boundaries to the receiver 
+ * during multi-file transmission
+ */
+typedef enum {
+    CONTROL_FRAME_NONE                  = 0x00,
+    CONTROL_FRAME_PREAMBLE              = 0xff,
+    CONTROL_FRAME_END_OF_TRANSMISSION   = 0xaa
+} dxwifi_control_frame_t;
+
+
+#endif // LIBDXWIFI_H
