e1dc15d2934eedeaf05d3507d817d9477e3bb4bd
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-26 11:01:45 -0800

    Add command line interface for Tx program
    The CLI give the user granular control over transmission settings
    at the command line. Currently only supports to transmission modes,
    streams and file/multifile transmission. Will add a directory
    event system in the future.

---
+++ tx-rx/dxwifi/tx/cli.h
@@ -0,0 +1,51 @@
+/**
+ *  cli.c
+ *  
+ *  DESCRIPTION: Command line interface for tx.c
+ * 
+ *  https://github.com/oresat/oresat-dxwifi-software
+ * 
+ */
+
+
+#include <libdxwifi/transmitter.h>
+
+
+typedef enum {
+    TX_FILE_MODE,
+    TX_STREAM_MODE,
+    TX_DIRECTORY_MODE,
+} tx_mode_t;
+
+// TODO this is defined arbitrarily, is there an upper limit to the number of 
+// Files to transmit at a time? 
+#define TX_CLI_FILE_MAX 1024
+
+typedef struct {
+    tx_mode_t           tx_mode;
+    char*               files[TX_CLI_FILE_MAX];
+    int                 verbosity;
+    unsigned            tx_delay;
+    const char*         device;
+    dxwifi_transmitter  tx;
+} cli_args;
+
+
+/**
+ *  DESCRIPTION:    Parse command line arguments into the cli_args struct
+ * 
+ *  ARGUMENTS:
+ * 
+ *      argc:       Number of command line arguments
+ * 
+ *      argv:       list of arguments
+ * 
+ *      out:        Pointer to allocated cli_args structure.
+ * 
+ *  RETURNS:
+ *      
+ *      int:        0 if arguments were parsed successfully
+ *     
+ *  
+ */
+int parse_args(int argc, char** argv, cli_args* out);
