40ae7421f47b2257e985058ef87645673faf3a22
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-02-27 14:54:03 -0800

    Finished rewrite of rx program
    Rx uses the receivers library module to capture packets that match
    a specified BPF expression. Rx comes with a robust CLI for configuring
    how capture is handled. Rx supports outputting data over a stream like
    stdout, a single file, or multiple files in a directory.

---
+++ tx-rx/dxwifi/rx/rx.c
@@ -0,0 +1,162 @@
+/**
+ *  rx.c
+ * 
+ *  DESCRIPTION: DxWiFi Receiver program
+ * 
+ *  https://github.com/oresat/oresat-dxwifi-software
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <linux/limits.h>
+
+#include <dxwifi/rx/cli.h>
+
+#include <libdxwifi/dxwifi.h>
+#include <libdxwifi/receiver.h>
+#include <libdxwifi/details/logging.h>
+
+
+dxwifi_receiver* receiver = NULL;
+
+void sigint_handler(int signum);
+
+void log_rx_stats(dxwifi_rx_stats stats);
+
+void receive(cli_args* args, dxwifi_receiver* rx);
+
+dxwifi_rx_state_t open_file_and_capture(const char* path, dxwifi_receiver* rx, bool append);
+
+void capture_in_directory(const char* dirname, const char* file_prefix, const char* file_suffix, dxwifi_receiver* rx);
+
+int main(int argc, char** argv) {
+    cli_args args = {
+        .rx_mode        = RX_STREAM_MODE,
+        .verbosity      = DXWIFI_LOG_OFF,
+        .append         = false,
+        .device         = "mon0",
+        .output_path    = ".",
+        .file_prefix    = "rx",
+        .file_extension = "cap",
+        .rx = {
+            .dispatch_count     = 32,
+            .capture_timeout    = -1, // No timeout
+            .packet_buffer_size = DXWIFI_RX_PACKET_BUFFER_SIZE_MAX,
+            .filter             = "wlan addr2 aa:aa:aa:aa:aa:aa",
+            .optimize           = true,
+            .snaplen            = DXWIFI_SNAPLEN_MAX,
+            .pb_timeout         = DXWIFI_DFLT_PACKET_BUFFER_TIMEOUT
+        }
+    };
+    receiver = &args.rx;
+
+    parse_args(argc, argv, &args);
+
+    init_logging();
+
+    set_log_level(DXWIFI_LOG_ALL_MODULES, args.verbosity);
+
+    init_receiver(receiver, args.device);
+
+    receive(&args, receiver);
+
+    close_receiver(receiver);
+
+    exit(0);
+}
+
+
+dxwifi_rx_state_t open_file_and_capture(const char* path, dxwifi_receiver* rx, bool append) {
+    int fd          = 0;
+    int open_flags  = O_WRONLY | O_CREAT | (append ? O_APPEND : 0);
+    mode_t mode     = S_IRWXU  | S_IRWXO;
+
+    dxwifi_rx_stats stats;
+    if((fd = open(path, open_flags, mode)) < 0) {
+        log_error("Failed to open file: %s", path);
+        stats.capture_state = DXWIFI_RX_ERROR;
+    }
+    else {
+        signal(SIGINT, sigint_handler);
+        receiver_activate_capture(rx, fd, &stats);
+        signal(SIGINT, SIG_DFL);
+        log_rx_stats(stats);
+        close(fd);
+    }
+    return stats.capture_state;
+}
+
+
+void capture_in_directory(const char* dirname, const char* file_prefix, const char* file_suffix, dxwifi_receiver* rx) {
+    int count = 0;
+    char path[PATH_MAX]; 
+
+    dxwifi_rx_state_t state = DXWIFI_RX_NORMAL;
+    while(state == DXWIFI_RX_NORMAL) {
+        sprintf(path, "%s/%s_%d.%s", dirname, file_prefix, count++, file_suffix);
+
+        state = open_file_and_capture(path, rx, false);
+    }
+}
+
+
+void receive(cli_args* args, dxwifi_receiver* rx) {
+
+    dxwifi_rx_stats stats;
+    switch (args->rx_mode)
+    {
+    case RX_STREAM_MODE:
+        signal(SIGINT, sigint_handler);
+        receiver_activate_capture(rx, STDOUT_FILENO, &stats);
+        signal(SIGINT, SIG_DFL);
+        log_rx_stats(stats);
+        break;
+
+    case RX_FILE_MODE:
+        open_file_and_capture(args->output_path, rx, args->append);
+        break;
+
+    case RX_DIRECTORY_MODE:
+        capture_in_directory(args->output_path, args->file_prefix, args->file_extension, rx);
+        break;
+    
+    default:
+        break;
+    }
+
+}
+
+
+void log_rx_stats(dxwifi_rx_stats stats) {
+    log_info(
+        "Receiver Capture Stats\n"
+        "\tTotal Payload Size:          %d\n"
+        "\tData Written Out:            %d\n"
+        "\tTotal Capture Size:          %d\n"
+        "\tTotal Blocks Lost:           %d\n"
+        "\tPackets Received:            %d\n"
+        "\tPackets Dropped (Kernel):    %d\n"
+        "\tPackets Dropped (NIC):       %d\n",
+        stats.total_payload_size,
+        stats.total_writelen,
+        stats.total_caplen,
+        stats.total_blocks_lost,
+        stats.pcap_stats.ps_recv,
+        stats.pcap_stats.ps_drop,
+        stats.pcap_stats.ps_ifdrop
+    );
+}
+
+
+void sigint_handler(int signum) {
+    // TODO need to come up with a better strategy to gracefully exit the application
+    signal(SIGINT, SIG_IGN);
+    receiver_stop_capture(receiver);
+    signal(SIGINT, sigint_handler);
+}
