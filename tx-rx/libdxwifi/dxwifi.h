61df8e426fb7c35bcab1f8bf7e4f4576e5173cfb
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-03-25 12:33:23 -0700

    Add versioning strings

--- tx-rx/libdxwifi/dxwifi.h
+++ tx-rx/libdxwifi/dxwifi.h
@@ -10,6 +10,28 @@
 #ifndef LIBDXWIFI_H
 #define LIBDXWIFI_H
 
+/************************
+ *  Versioning
+ ***********************/
+
+#define DXWIFI_VERSION_MAJOR    0
+#define DXWIFI_VERSION_MINOR    1
+#define DXWIFI_VERSION_PATCH    0
+#define DXWIFI_VERSION_RELEASE  "alpha"
+
+#ifndef DXWIFI_BUILD_TYPE
+#define DXWIFI_BUILD_TYPE       "debug"
+#endif
+
+#define STRINGIFY(x)  STRINGIFY_(x)
+#define STRINGIFY_(x) #x
+
+#define DXWIFI_VERSION      STRINGIFY(DXWIFI_VERSION_MAJOR) "." \
+                            STRINGIFY(DXWIFI_VERSION_MINOR) "." \
+                            STRINGIFY(DXWIFI_VERSION_PATCH) "-" \
+                            DXWIFI_VERSION_RELEASE          "-" \
+                            DXWIFI_BUILD_TYPE
+
 
 /************************
  *  Constants
